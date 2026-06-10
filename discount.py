a64b341ddf22f7c452a94c3bc1b028c1b707fd09
Raimon Esteve <raimonesteve@gmail.com>
2026-06-10 14:16:11 +0200

    pyflakes

--- discount.py
+++ discount.py
@@ -1,7 +1,6 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from decimal import Decimal
-
 from trytond.modules.product import price_digits
 
 
